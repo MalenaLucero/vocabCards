1d6f937a7647bfccd8d47d1541dfedd41dd3de81
MalenaLucero <luceromalena@hotmail.com>
2021-07-18 21:06:38 -0300

    vocab added

--- data/expressions/120621expressions.js
+++ data/expressions/120621expressions.js
@@ -37,5 +37,138 @@ const expressions120621 = [
             }
         ],
         tags: []
+    }, {
+        word: "待遇",
+        reading: "たいぐう",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["treatment; reception; service​", "working conditions; salary; pay; remuneration"],
+        japaneseMeaning: ["人をもてなすこと。あしらい。「待遇の良いホテル」「大切な客として待遇する」", "給与・勤務時間など、雇用者の勤労者に対する取り扱い。処遇。「待遇を改善する」「高給で待遇する」"],
+        exampleSentences: [
+            {
+                sentence: "悪い仕事をしているってよりはどちかというと従業員に対して、社員に対して、待遇がよくないとか、従業員に対しての扱いが悪い会社のことをブラック企業とよく言いますよね",
+                source: {
+                    name: "EP-104 ブラック企業と残業文化 Exploitative enterpriseand Overwork culture(N2-N1)",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["仕事"]
+    }, {
+        word: "繁忙",
+        reading: "はんぼう",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["pressure of business; busy"],
+        japaneseMeaning: ["仕事が多くて忙しいこと。また、そのさま。「―を極めた生活」「日々の―から逃れる」「―な時期」"],
+        exampleSentences: [
+            {
+                sentence: "日本だけじゃなくて世界中、ま、人気の会社とかやっぱり繁忙期、会社の繁忙期、繁忙期っていうのは忙しいシーズンですね",
+                source: {
+                    name: "EP-104 ブラック企業と残業文化 Exploitative enterpriseand Overwork culture(N2-N1)",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["仕事"]
+    }, {
+        word: "店舗",
+        reading: "てんぽ",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["shop; store"],
+        japaneseMeaning: ["商品を並べて売るための建物。みせ。「―を広げる」「大型―」"],
+        exampleSentences: [
+            {
+                sentence: "あと、多店舗展開している会社ですね。多店舗展開というのは店舗を持っている会社ということですね。店舗っていうのは、まあ、店。例えば、飲食店とか、デパート、スーパーとかもそうだし、お店がありますよね、とか、かなあ",
+                source: {
+                    name: "EP-104 ブラック企業と残業文化 Exploitative enterpriseand Overwork culture(N2-N1)",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["仕事"]
+    }, {
+        word: "部署",
+        reading: "ぶしょ",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["one's post; one's station; department; bureau; section"],
+        japaneseMeaning: ["それぞれに役割や分担を決めること。また、その役割や担当した場所。持ち場。「部署を移る」「部署に就く」"],
+        exampleSentences: [
+            {
+                sentence: "一日で店舗を回り切れないから、夜遅くまで車でその店舗に行ってチェックしたりしてたっていうのがあって、あとはその部署の上司が、まあ、パワハラじゃないけど、その上司がすごくもうワークホリックの人だったんだって",
+                source: {
+                    name: "EP-104 ブラック企業と残業文化 Exploitative enterpriseand Overwork culture(N2-N1)",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["仕事"]
+    }, {
+        word: "浸透",
+        reading: "しんとう",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["permeation (of thought, ideology, culture, etc.); infiltration (e.g. of ideas); spread; penetration (e.g. into a market); pervasion"],
+        japaneseMeaning: ["水などが、しみとおること。「雨水が地下に浸透する」「堤防の浸透破壊」", "思想・風潮・雰囲気などがしだいに広い範囲に行きわたること。「新しい生活様式が国民に浸透する」"],
+        exampleSentences: [
+            {
+                sentence: "だから休むのは許されないみたいな、その上司の考えがその部下にも、なんだろう、浸透しているってか、もはや洗脳みたいな感じなんだけど、してて、逆らえないということがあって",
+                source: {
+                    name: "EP-104 ブラック企業と残業文化 Exploitative enterpriseand Overwork culture(N2-N1)",
+                    link: ""
+                }
+            }
+        ],
+        tags: []
+    }, {
+        word: "発散",
+        reading: "はっさん",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["emission; emanation; radiation; diffusion; dispersion​", "letting out (feelings); venting"],
+        japaneseMeaning: ["内部にたまったものが外部へ散らばって出ること。また、外部へ散らばり出ること。「ストレスを発散させる」", "一点から出た光が広がって進むこと"],
+        exampleSentences: [
+            {
+                sentence: "すごく太って、もう食べることでストレスを発散するっていうふうになっちゃって、もうすごい太ってたんですよ",
+                source: {
+                    name: "EP-104 ブラック企業と残業文化 Exploitative enterpriseand Overwork culture(N2-N1)",
+                    link: ""
+                }
+            }
+        ],
+        tags: []
+    }, {
+        word: "犠牲",
+        reading: "ぎせい",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["sacrifice; victim"],
+        japaneseMeaning: ["人や動物を生きたまま神に供えること。また、その供え物。「―をささげる」", "ほかの人やある物事のために生命や名誉・利益を投げ捨てること。また、その人。犠牲 (ぎせい) 。「企業間戦争の―になる」"],
+        exampleSentences: [
+            {
+                sentence: "どんなに有名な会社でも、有名な会社で働く意味、自分の体を犠牲にしてまで有名な会社で働かなきゃ行けない意味なんて一ミリもないからねと思います",
+                source: {
+                    name: "EP-104 ブラック企業と残業文化 Exploitative enterpriseand Overwork culture(N2-N1)",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["仕事"]
     }
 ]
