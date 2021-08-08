902dbc2761224ec274c369b424bd643d27045519
MalenaLucero <luceromalena@hotmail.com>
2021-08-08 13:08:27 -0300

    search by meaning and reading

--- search/search.js
+++ search/search.js
@@ -12,40 +12,71 @@ const clean = () => {
     searchInput.value = ''
 }
 
-const searchRawData = (type, searchInput) => {
+const searchInRawData = (type, property, searchInput) => {
     const results = []
     Object.keys(rawData[type]).forEach(key => {
         rawData[type][key].forEach(obj => {
-            const property = type === 'expressions' ? 'word' : 'kanji'
-            if(obj[property].includes(searchInput)) {
-                const data = { data: obj, date: key }
-                results.push(data)
+            if (typeof obj[property] === 'string') {
+                if(obj[property].includes(searchInput)) {
+                    const data = { data: obj, date: key }
+                    results.push(data)
+                }
+            } else if (Array.isArray(obj[property])) {
+                const isPresent = obj[property].find(meaning => meaning.includes(searchInput))
+                if (isPresent) {
+                    const data = { data: obj, date: key }
+                    results.push(data)
+                }
             }
         })
     })
     return results
 }
 
-const search = () => {
-    cleanInnerHtml('searchResult')
-    const searchInput = document.getElementById('searchInput').value
-    insertTextAsElement('searchResult', 'h2', 'Results')
+const showExpressionResults = results => {
     insertTextAsElement('searchResult', 'h3', 'Expressions')
-    const expressionResults = searchRawData('expressions', searchInput)
-    if (expressionResults.length > 0) {
-        expressionResults.forEach(result => {
+    if (results.length > 0) {
+        results.forEach(result => {
            showExpressionData('searchResult', result) 
         })
     } else {
         insertTextAsElement('searchResult', 'p', 'Nothing found')
     }
+}
+
+const showKanjiResults = results => {
     insertTextAsElement('searchResult', 'h3', 'Kanji')
-    const kanjiResults = searchRawData('kanjis', searchInput)
-    if (kanjiResults.length > 0) {
-        kanjiResults.forEach(result => {
+    if (results.length > 0) {
+        results.forEach(result => {
             showKanjiData('searchResult', result)
         })
     } else {
         insertTextAsElement('searchResult', 'p', 'Nothing found')
     }
+}
+
+const cleanAndGetInputValue = () => {
+    cleanInnerHtml('searchResult')
+    insertTextAsElement('searchResult', 'h2', 'Results')
+    return document.getElementById('searchInput').value
+}
+
+const search = () => {
+    const searchInput = cleanAndGetInputValue()
+    const expressionResults = searchInRawData('expressions', 'word', searchInput)
+    showExpressionResults(expressionResults)
+    const kanjiResults = searchInRawData('kanjis', 'kanji', searchInput)
+    showKanjiResults(kanjiResults)
+}
+
+const searchByExpressionReading = () => {
+    const searchInput = cleanAndGetInputValue()
+    const expressionResults = searchInRawData('expressions', 'reading', searchInput)
+    showExpressionResults(expressionResults)
+}
+
+const searchByEnglishMeaning = () => {
+    const searchInput = cleanAndGetInputValue()
+    const expressionResults = searchInRawData('expressions', 'englishMeaning', searchInput)
+    showExpressionResults(expressionResults)
 }
