393428b3acb862830214f27caac4966784ac4e0c
MalenaLucero <luceromalena@hotmail.com>
2021-08-31 21:43:45 -0300

    vocab added

---
+++ data/expressions/210821expressions.js
@@ -0,0 +1,269 @@
+const expressions210821 = [
+    {
+        word: "海原",
+        reading: "うなばら",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["ocean; sea; the deep"],
+        japaneseMeaning: ["広々とした海。「青海原」", "池や湖の広い水面。"],
+        exampleSentences: [
+            {
+                sentence: "枯れ果てた海原に / 突き刺さった三日月よ",
+                source: {
+                    name: "birth - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }, {
+        word: "三日月",
+        reading: "みかづき",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["new moon; crescent moon"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "枯れ果てた海原に / 突き刺さった三日月よ",
+                source: {
+                    name: "birth - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }, {
+        word: "哀れみ",
+        reading: "あわれみ",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["pity; compassion"],
+        japaneseMeaning: ["かわいそうに思う心。慈悲。同情。「―をかける」"],
+        exampleSentences: [
+            {
+                sentence: "哀れみと安堵感 / 混じり合ったパレットで",
+                source: {
+                    name: "birth - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }, {
+        word: "安堵感",
+        reading: "あんどかん",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["sense of relief; feeling of relief"],
+        japaneseMeaning: ["気がかりなことが除かれ、安心すること。「安堵の胸をなでおろす」「無事を聞いて安堵した」"],
+        exampleSentences: [
+            {
+                sentence: "哀れみと安堵感 / 混じり合ったパレットで",
+                source: {
+                    name: "birth - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }, {
+        word: "寂れる",
+        reading: "さびれる",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["to decline (in prosperity); to become deserted; to become desolate"],
+        japaneseMeaning: ["活気がなくなって寂しくなる。ひっそりする。「―・れた冬の浜辺」「―・れた町」", "勢いが衰える。「虫の音が―・れる」"],
+        exampleSentences: [
+            {
+                sentence: "寂れた路地の古い時計は / 終わりのない時を刻んで",
+                source: {
+                    name: "Blues in June - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }, {
+        word: "路地",
+        reading: "ろじ",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["alley; alleyway; lane"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "寂れた路地の古い時計は / 終わりのない時を刻んで",
+                source: {
+                    name: "Blues in June - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }, {
+        word: "焦がす",
+        reading: "こがす",
+        transitivity: {
+            type: "他動詞",
+            opposingWord: "焦げる"
+        },
+        englishMeaning: ["to burn; to scorch; to singe; to char"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "むらさき色の煙は / この街を焦がしてゆく",
+                source: {
+                    name: "Blues in June - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }, {
+        word: "口ずさむ",
+        reading: "くちずさむ",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["to hum (a tune); to sing to oneself; to croon"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "ため息代わりにひとり / ブルースを口ずさむ",
+                source: {
+                    name: "Blues in June - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }, {
+        word: "草臥れる",
+        reading: "くたびれる",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["to get tired; to become exhausted; to grow weary​", "to become worn out; to become battered (from long use)​"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "くたびれた古いジャケットに / ひそむはあの頃の夢",
+                source: {
+                    name: "Blues in June - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }, {
+        word: "霧",
+        reading: "きり",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["fog; mist​"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "霧にまみれた記憶が / 今でも耳に残る",
+                source: {
+                    name: "Blues in June - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }, {
+        word: "夜汽車",
+        reading: "よぎしゃ",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["night train"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "満月の夜汽車に乗って / 赤いランプに火を灯して",
+                source: {
+                    name: "Blues in June - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }, {
+        word: "灯す",
+        reading: "ともす",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["to light (a candle, lamp, etc.); to turn on"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "満月の夜汽車に乗って / 赤いランプに火を灯して",
+                source: {
+                    name: "Blues in June - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }, {
+        word: "纏う",
+        reading: "まとう",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["to put on; to wear; to be clad in"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "街角には愛が溢れ / カラフルな夢を纏って",
+                source: {
+                    name: "ポラリス - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }, {
+        word: "星屑",
+        reading: "ほしくず",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["stardust"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "今夜の空ロマンチック / 星屑のイルミネーション",
+                source: {
+                    name: "ポラリス - 大橋トリオ",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌"]
+    }
+]
