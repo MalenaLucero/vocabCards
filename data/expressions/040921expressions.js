8c838920c200847c851f20f238f1fcfe7b297f20
MalenaLucero <luceromalena@hotmail.com>
2021-09-06 20:50:53 -0300

    vocab added

---
+++ data/expressions/040921expressions.js
@@ -0,0 +1,41 @@
+const expressions040921 = [
+    {
+        word: "携える",
+        reading: "たずさえる",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["to carry in one's hand; to carry with one; to have on one's person; to bear"],
+        japaneseMeaning: ["手にさげて、また、身につけて持つ。「手土産を―・えて訪問する」"],
+        exampleSentences: [
+            {
+                sentence: "心に刺さったままの傷を / 携えて日々は続いていく",
+                source: {
+                    name: "given - Opening",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌", "given"]
+    }, {
+        word: "春夏秋冬",
+        reading: "しゅんかしゅうとう",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: [],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "雨 晴れ 曇り / 春夏秋冬 365日 / どれも君が宿る",
+                source: {
+                    name: "given - Opening",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌", "given"]
+    }
+]
