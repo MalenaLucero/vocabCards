d3c330afcc0d836298bdcb9b5bfeaec40c212acf
MalenaLucero <luceromalena@hotmail.com>
2021-09-28 22:13:17 -0300

    vocab added

---
+++ data/expressions/180921expressions.js
@@ -0,0 +1,98 @@
+const expressions180921 = [
+    {
+        word: "古びる",
+        reading: "ふるびる",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["to look old; to get old"],
+        japaneseMeaning: ["古くなる。古くさくなる。「―・びた洋服"],
+        exampleSentences: [
+            {
+                sentence: "忘れた物を取りに帰るように / 古びた思い出の埃を払う",
+                source: {
+                    name: "Lemon - 米津玄師",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌", "米津玄師"]
+    }, {
+        word: "昏い",
+        reading: "くらい",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: [],
+        japaneseMeaning: ["光が弱い。光が少なくて、物がよく見えない。「辺りが―・い」「―・い夜道」⇔明るい。"],
+        exampleSentences: [
+            {
+                sentence: "言えずに隠してた昏い過去も / あなたがいなきゃ永遠に昏いまま",
+                source: {
+                    name: "Lemon - 米津玄師",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌", "米津玄師"]
+    }, {
+        word: "なぞる",
+        reading: "",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["to trace (drawing); to follow"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "暗闇であなたの背をなぞった / その輪郭を鮮明に覚えている",
+                source: {
+                    name: "Lemon - 米津玄師",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌", "米津玄師"]
+    }, {
+        word: "輪郭",
+        reading: "りんかく",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["contour; outline; border; silhouette"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "暗闇であなたの背をなぞった / その輪郭を鮮明に覚えている",
+                source: {
+                    name: "Lemon - 米津玄師",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌", "米津玄師"]
+    }, {
+        word: "片方",
+        reading: "かたほう",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["other side; the other party"],
+        japaneseMeaning: ["対になっているものの一つ。かたっぽ。かたっぽう。片一方。「手袋を片方なくした」"],
+        exampleSentences: [
+            {
+                sentence: "切り分けた果実の片方の様に / 今でもあなたはわたしの光",
+                source: {
+                    name: "Lemon - 米津玄師",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["歌", "米津玄師"]
+    }
+]
