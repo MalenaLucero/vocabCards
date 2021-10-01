1fd933061ba5cacdb7b70e09ae2411e1a1f0d64b
MalenaLucero <luceromalena@hotmail.com>
2021-09-30 22:48:15 -0300

    vocab added

--- data/expressions/180921expressions.js
+++ data/expressions/180921expressions.js
@@ -94,5 +94,290 @@ const expressions180921 = [
             }
         ],
         tags: ["歌", "米津玄師"]
+    }, {
+        word: "小まめ",
+        reading: "こまめ",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["diligent; earnest; attentive to detail; brisk (working, moving); frequent (e.g. checking)"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:03:50,271（立夏）ちょっとしたことですぐ狂うんだよ  ケース入れるだけでもペグずれるし    チューニングはこまめにしろ",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "捲る",
+        reading: "まくる",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["to do over and over again; to do relentlessly; to do with reckless abandon"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:05:35,251 最初は　あんま難しいこと考えないで　弾きまくったほうがいいし",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "合間",
+        reading: "あいま",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["interval; break; pause; spare moment"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:07:09,387 練習の合間なら　お前もギター弾いていいって",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "一致",
+        reading: "いっち",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["coincidence; agreement; union; match"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:07:50,428 着くまでに ちゃんと２人の顔と名前 一致させとけよ",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "身をもって",
+        reading: "身をもって",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["with one's own body; by one's own action; through one's own experience; firsthand"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:10:25,791（立夏）身をもって失敗したほうが覚えるし",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "労る",
+        reading: "いたわる",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["to pity; to sympathize with; to sympathise with; to treat with sympathy; to console; to be kind to; to appreciate"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:10:28,044（春樹）そうかもだけど　俺らの耳もいたわって",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "尽くす",
+        reading: "つくす",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["to devote oneself (to); to do one's utmost (for); to serve; to work (for a cause)"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:12:14,066（春樹）実は結構　尽くし上手だと思うんだよね",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "思春期",
+        reading: "ししゅんき",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["puberty; adolescence"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:12:37,715 生意気なのはあれだ 単に思春期",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "庇護欲",
+        reading: "ひごよく",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["desire to protect"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:13:01,238（秋彦）あれは尽くし上手っつーより 庇護欲が強いっつーか 裏を返せば あるいは…",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "意思疎通",
+        reading: "いしそつう",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["(coming to a) mutual understanding; understanding each other"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:13:36,315（立夏）意思疎通ができてるのか よく分からない",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "単発",
+        reading: "たんぱつ",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["one-shot (e.g. story); non-serial; one-off; single occasion"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:15:56,497 あと単発で バイク便とか イベントのウエイターとか 警備も たまにやるな",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "辟易",
+        reading: "へきえき",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["to be fed up (with); to be tired (of); to be bored (with); to be unable to bear"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:16:39,123（立夏）弾きやすそうな ポップな邦楽ロックの コピバン高校生見て へきえきしたくないから",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "動機",
+        reading: "どうき",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["motive; incentive"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:18:51,421 人が何か上達する様は 見ていて気持ちがいい けれど いまだに こいつは謎だ 音楽への動機も よく分からない",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "似つかわしい",
+        reading: "につかわしい",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["suitable; appropriate; becoming; fitting"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:19:02,558 それに こいつのギターが 初心者の… しかも高校生には およそ 似つかわしくないという違和感",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
+    }, {
+        word: "渋い",
+        reading: "しぶい",
+        transitivity: {
+            type: "",
+            opposingWord: ""
+        },
+        englishMeaning: ["austere; elegant (and unobtrusive); refined; quiet (and simple); sober; sombre; subdued; tasteful (in a quiet way); understated"],
+        japaneseMeaning: [],
+        exampleSentences: [
+            {
+                sentence: "00:19:10,941 素人が こんな渋いの買うか？いくらすんだよ",
+                source: {
+                    name: "Given - 02",
+                    link: ""
+                }
+            }
+        ],
+        tags: ["アニメ", "given"]
     }
 ]
